9d1df533c0df60be4150f1e85c54daa70ffcb9c7
George V <georgev22.github@gmail.com>
2023-03-18 22:48:58 +0200

    fix(SkinHandler): Fixed legacy SkinHandler index

--- bukkit/src/main/java/com/georgev22/skinoverlay/handler/handlers/SkinHandler_Legacy.java
+++ bukkit/src/main/java/com/georgev22/skinoverlay/handler/handlers/SkinHandler_Legacy.java
@@ -233,7 +233,7 @@ public class SkinHandler_Legacy extends SkinHandler_ {
             if (dataWatcher != null) {
                 Object dataWatcherObject;
                 try {
-                    dataWatcherObject = invokeConstructor(BukkitMinecraftUtils.MinecraftReflection.getNMSClass("DataWatcherObject"), 16, fetchField(getNMSClass("DataWatcherRegistry"), null, "a"));
+                    dataWatcherObject = invokeConstructor(BukkitMinecraftUtils.MinecraftReflection.getNMSClass("DataWatcherObject"), BukkitMinecraftUtils.MinecraftVersion.getCurrentVersion().isBelow(BukkitMinecraftUtils.MinecraftVersion.V1_16_R1) ? 13 : 16, fetchField(getNMSClass("DataWatcherRegistry"), null, "a"));
                 } catch (ClassNotFoundException e) {
                     callback.onFailure(e);
                     return;
