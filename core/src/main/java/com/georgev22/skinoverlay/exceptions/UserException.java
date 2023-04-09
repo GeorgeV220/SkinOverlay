99bbb343263e01d23f021b8f02b379be102ca816
George V <georgev22.github@gmail.com>
2023-04-09 22:38:44 +0300

    feat(Exception): Added UserException

---
+++ core/src/main/java/com/georgev22/skinoverlay/exceptions/UserException.java
@@ -0,0 +1,21 @@
+package com.georgev22.skinoverlay.exceptions;
+
+public class UserException extends RuntimeException {
+
+    public UserException(String message) {
+        super(message);
+    }
+
+    public UserException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserException(Throwable cause) {
+        super(cause);
+    }
+
+    public UserException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+}
