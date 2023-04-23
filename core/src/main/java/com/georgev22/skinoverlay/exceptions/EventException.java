86b6d63a950b3a6860a5b90c6a313cfa4393f8df
George V <georgev22.github@gmail.com>
2023-04-23 17:39:32 +0300

    feat(Exception): Added EventException

---
+++ core/src/main/java/com/georgev22/skinoverlay/exceptions/EventException.java
@@ -0,0 +1,20 @@
+package com.georgev22.skinoverlay.exceptions;
+
+public class EventException extends RuntimeException {
+
+    public EventException(String message) {
+        super(message);
+    }
+
+    public EventException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EventException(Throwable cause) {
+        super(cause);
+    }
+
+    public EventException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
