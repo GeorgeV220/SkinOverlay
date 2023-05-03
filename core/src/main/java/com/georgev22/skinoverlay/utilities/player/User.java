afe2613e729abed53df0e8f7468265d5b94d5e20
George V <georgev22.github@gmail.com>
2023-05-03 16:24:16 +0300

    fix(User): Added serial version

--- core/src/main/java/com/georgev22/skinoverlay/utilities/player/User.java
+++ core/src/main/java/com/georgev22/skinoverlay/utilities/player/User.java
@@ -1,11 +1,15 @@
 package com.georgev22.skinoverlay.utilities.player;
 
-import com.georgev22.library.utilities.EntityManager;
+import com.georgev22.library.utilities.EntityManager.Entity;
 import com.georgev22.skinoverlay.handler.Skin;
 
+import java.io.Serial;
 import java.util.UUID;
 
-public class User extends EntityManager.Entity {
+public class User extends Entity {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     public User(UUID uuid) {
         super(uuid);
